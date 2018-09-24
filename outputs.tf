4a1db2245df71cdc0a345e0fcf90fce7bf2fb4d8
J. Taylor Mayfield <jtaylormayfield@yahoo.com>
2018-09-24 16:59:05 -0500

    Add VPC id output.

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,14 @@ output "public_nat_ip" {
   value = "${module.routes.ngw_public_ip}"
 }
 
-
 output "public_subnet_ids" {
   value = "${module.subnets.public_ids}"
 }
 
 output "private_subnet_ids" {
   value = "${module.subnets.private_ids}"
+}
+
+output "vpc_id" {
+  value = "${module.vpc.id}"
 }
