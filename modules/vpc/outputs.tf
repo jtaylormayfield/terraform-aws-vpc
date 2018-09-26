ff460b17e4be4dfd7ec04d6a6fdf37ef461378d4
J. Taylor Mayfield <jtaylormayfield@yahoo.com>
2018-09-26 15:01:59 -0500

    Expose peering connection ID output.

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,7 @@
 output "id" {
   value = "${aws_vpc.vpc.id}"
 }
+
+output "peering_connection_id" {
+  value = "${local.peering_enabled ? aws_vpc_peering_connection.main.id : ""}"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,24 @@
+output "peering_connection_id" {
+  description = "Peering connection ID. Empty if peering is disabled."
+  value = "${module.vpc.peering_connection_id}"
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs."
+  value = "${module.subnets.private_ids}"
+}
+
 output "public_nat_ip" {
+  description = "Public NAT gateway IP."
   value = "${module.routes.ngw_public_ip}"
 }
 
 output "public_subnet_ids" {
+  description = "Public subnet IDs."
   value = "${module.subnets.public_ids}"
 }
 
-output "private_subnet_ids" {
-  value = "${module.subnets.private_ids}"
-}
-
 output "vpc_id" {
+  description = "VPC ID."
   value = "${module.vpc.id}"
 }
